aa6e0a552091f82bfdb56dfdeed888ce0162ecf3
gupta2022 <44429156+gupta2022@users.noreply.github.com>
2019-06-05 22:14:28 +0530

    Create AddTwoNumbers.cpp

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//int is used to declare variables of int datatype that are used to take user input
+ int a,b;
+ //cin is used to take user input
+ cin>>a>>b;
+ //prints the sum a+b
+ cout<<"the sum is"<<a+b;
+ return 0;
+}
