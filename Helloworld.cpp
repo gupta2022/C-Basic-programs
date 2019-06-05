13b436f3c9a76421a51097995591c8ca09a169c3
gupta2022 <44429156+gupta2022@users.noreply.github.com>
2019-06-05 22:09:48 +0530

    Create Helloworld.cpp

---
+++ Helloworld.cpp
@@ -0,0 +1,11 @@
+// THESE // WILL BE USED TO GIVE COMMENTS AND ARE NOT PROCESSED BY THE COMPILER
+//Some of the bsic header files
+#include<iostream>
+//This add the keyword std to some of the pre defined functions
+using namespace std;
+int main()
+{
+//Prints Hello World
+ cout<<"Hello World"<<endl;
+ return 0;
+}
